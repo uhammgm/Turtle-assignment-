2eeb3180999d5b8549964b509b22bb2db768c507
uhammgm <uhammgm@stu.ccsdut.org>
2023-03-06 17:05:44 -0700

    I did a whole bunch. I finished the snowman and am now started on the hat.

--- snowman.py
+++ snowman.py
@@ -1,8 +1,26 @@
 import turtle
 
-turtle.color("black")
+turtle.setheading(225)
 turtle.penup()
-turtle.goto(0, -150)
+turtle.goto(-50, -150)
 turtle.pendown()
-turtle.circle(50)
+turtle.circle(50, 270)
+turtle.penup()
+#base done
+turtle.goto(-36, -100)
+turtle.pendown()
+turtle.setheading(213)
+turtle.circle(40, 295)
+#middle done
+turtle.penup()
+turtle.goto(-35,-58)
+turtle.pendown()
+turtle.setheading(225)
+turtle.circle(30, 270)
+#top done
+turtle.width(3)
+turtle.color("green")
+turtle.goto(-50,-58)
+turtle.goto(-50, -50)
+turtle.hideturtle()
 turtle.done()
